dc1adc7dd699ac6182212978df8096282cfdef63
Abhijith Balagurusamy <crookedberet@gmail.com>
2018-11-03 13:06:14 -0400

    message class finish

--- Project/src/Message.java
+++ Project/src/Message.java
@@ -1,8 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Message {
     String text;
+    PrintWriter out;
     public Message(String t) {
         text = t;
     }
-
-
+    public void send(Socket socket) throws Exception {
+        out = new PrintWriter(socket.getOutputStream());
+        out.println(text);
+    }
 }
