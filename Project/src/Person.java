3b411af68771d657431cb9ff0f9051152232e919
vikramanantha <vikramanantha@gmail.com>
2018-11-03 13:26:33 -0400

    Finiished PRSON

--- Project/src/Person.java
+++ Project/src/Person.java
@@ -1,3 +1,22 @@
-public class Person {
+import java.net.Socket;
 
+public class Person {
+    String name;
+    Socket socket;
+
+    public Person(String n) {
+        name = n;
+    }
+
+
+    public void SendMessage(Person Dude, String text, Server serv, Person me) throws Exception{
+        Message m = new Message(text);
+        m.send(socket);
+
+
+    }
+
+    public String RecieveMessage(Person Dude, Server serv) {
+        m.recieve(socket);
+    }
 }
